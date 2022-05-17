03a244c34f500e874cc011893a55413aad1a849e
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-17 11:25:04 -0400

    Adding a Block class to find out where each of the 4 points of our block is on our grid at any given moment

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-
 const grid = document.querySelector('.grid')
-const block = document.createElement('div')
-block.classList.add('block')
-grid.appendChild(block)
+const blockWidth = 100
+const blockHeight = 20
+
+
+
+const addBlock = () => {
+  const block = document.createElement('div')
+  block.classList.add('block')
+  block.style.left = '100px'
+  grid.style.bottom = '50px'
+  grid.appendChild(block)
+}
+
+addBlock()
+
+
+// Create Block 
+
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis]
+    this.bottomRight = [xAxis + blockWidth, yAxis]
+    this.topLeft = [xAxis, yAxis + blockHeight]
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+  }
+}
